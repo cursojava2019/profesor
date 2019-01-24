4278140879790e8248ed416361d80955772e875e
David <dtello@forticsoluciones.es>
2019-01-24 18:38:15 +0100

    fichero bbdd

---
+++ academiaProfesor/resources/bbdd.sql
@@ -0,0 +1,32 @@
+CREATE TABLE public.alumno
+(
+    nombre character varying(100) COLLATE pg_catalog."default",
+    apellido1 character varying(100) COLLATE pg_catalog."default",
+    apellido2 character varying(100) COLLATE pg_catalog."default",
+    nif character varying(9) COLLATE pg_catalog."default",
+    telefono character varying(9) COLLATE pg_catalog."default",
+    correo character varying(100) COLLATE pg_catalog."default",
+    repetidor boolean NOT NULL,
+    fechaalta character varying(10) COLLATE pg_catalog."default",
+    fechabaja character varying(10) COLLATE pg_catalog."default",
+    observaciones character varying(100) COLLATE pg_catalog."default",
+    id bigint NOT NULL DEFAULT nextval('alumno_id_seq'::regclass),
+    CONSTRAINT alumno_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE public.profesores
+(
+    id bigint NOT NULL DEFAULT nextval('profesores_id_seq'::regclass),
+    nombre character varying(100) COLLATE pg_catalog."default",
+    apellido1 character varying(100) COLLATE pg_catalog."default",
+    apellido2 character varying(100) COLLATE pg_catalog."default",
+    nif character varying(9) COLLATE pg_catalog."default",
+    telefono character varying(9) COLLATE pg_catalog."default",
+    correo character varying(100) COLLATE pg_catalog."default",
+    titulacion character varying(100) COLLATE pg_catalog."default"
+)
+
+
+
+
